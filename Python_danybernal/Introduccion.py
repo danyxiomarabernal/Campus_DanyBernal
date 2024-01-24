14047296697b9491656698104969c3de6dd51ebf
yuliethCor08 <yuliethcorrea08@gmail.com>
2024-01-23 21:25:00 -0500

    ejercio introduccion

---
+++ Python_danybernal/Introduccion.py
@@ -0,0 +1,10 @@
+z=3
+if z%2 ==0 :
+    print("z is divisible by 2")    #false
+elif z%3 ==0 :
+    print("z is divisible by 3")    #true
+else:
+    print("z is neither divisible by 2 nor by 3")
+
+
+ 
